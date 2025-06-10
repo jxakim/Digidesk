f0898cc7e1185ef3f59b141224a7470658f127de
Jxakim <joakim.solvang.christiansen@gmail.com>
2025-06-10 12:45:01 +0200

    Added admin route to navbar

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { Navigate } from 'react-router-dom';
 import '../styling/navbar.css';
 
 function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
+
+  const [isAuthenticated, setIsAuthenticated] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('/api/verify', {
+      credentials: 'include',
+    })
+      .then(res => {
+        if (res.ok) {
+          setIsAuthenticated(true);
+        } else {
+          setIsAuthenticated(false);
+        }
+      })
+      .catch(() => setIsAuthenticated(false))
+      .finally(() => setLoading(false));
+  }, []);
 
   return (
     <div id="navbar">
@@ -18,6 +37,7 @@ function Navbar() {
         <div id="navbar-right" className={menuOpen ? 'show' : ''}>
             <a href="/Problemer">Problemer</a>
             <a href="/Kontakt">Kontakt oss</a>
+            {isAuthenticated && <a href="/admin">Admin</a>}
         </div>
     </div>
   );
